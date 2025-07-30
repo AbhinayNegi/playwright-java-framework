f6550d7b1139d607f1cce92de7ef49a822f63383
Abhiany <negia394@gmail.com>
2025-07-30 22:40:58 +0530

    Initial commit

---
+++ src/main/java/org/abhinay/App.java
@@ -0,0 +1,13 @@
+package org.abhinay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
